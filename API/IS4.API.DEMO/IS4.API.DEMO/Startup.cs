059533439eb29648ece98f32d42fa47bbaa07157
Nancy Chiang <nancy.chih@kkday.com>
2018-08-13 14:01:31 +0800

    upd

--- API/IS4.API.DEMO/IS4.API.DEMO/Startup.cs
+++ API/IS4.API.DEMO/IS4.API.DEMO/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Security;
@@ -15,12 +16,16 @@ namespace IS4.API.DEMO {
     public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        //获取应用程序的当前工作目录。  
+        string LocaPath = Directory.GetCurrentDirectory();  
+
         public void ConfigureServices(IServiceCollection services) {
             services.AddIdentityServer()
                     // .AddDeveloperSigningCredential()
                 //.AddSigningCredential(new X509Certificate2(@"/etc/nginx/ssl/socialnetwork.pfx", "2wsx3edc"))
-               .AddSigningCredential(new X509Certificate2(@"/Users/jiangzhimin/socialnetwork.pfx", "2wsx3edc"))
-              
+
+               .AddSigningCredential(new X509Certificate2(LocaPath + "/Pfx/socialnetwork.pfx", "2wsx3edc"))
                .AddTestUsers(InMemoryConfiguration.Users().ToList())
                .AddInMemoryClients(InMemoryConfiguration.Clients())
                .AddInMemoryApiResources(InMemoryConfiguration.ApiResources());
