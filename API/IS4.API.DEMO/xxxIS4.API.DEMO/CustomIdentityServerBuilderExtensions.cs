f2bf142003abb8b0257e66d5529f89c6924648e7
Nancy Chiang <nancy.chih@kkday.com>
2018-08-22 14:05:53 +0800

    門票期限設為一個月

---
+++ API/IS4.API.DEMO/xxxIS4.API.DEMO/CustomIdentityServerBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using KKday.API.IS4.Server.Models.Repository;
+using IS4.API.DEMO.Models.Repository;
+
+namespace KKday.API.IS4.Server {
+    public static class CustomIdentityServerBuilderExtensions {
+        public static IIdentityServerBuilder AddCustomUserStore
+        (this IIdentityServerBuilder builder) {
+            builder.Services.AddSingleton<IUserRepository, UserRepository>();
+            builder.AddProfileService<CustomProfileService>();
+            builder.AddResourceOwnerValidator<CustomResourceOwnerPasswordValidator>();
+
+            return builder;
+        }
+    }
+}
