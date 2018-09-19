1e90d5e4979eb300d2add6ef3bae7b7adf95e7ba
luluchiu016 <lulu.chiu@kkday.com>
2018-09-19 17:57:38 +0800

    add lulu version

---
+++ API/KKday.API.WMS/KKday.API.WMS/Models/DataModel/Supplier/SupplierModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace KKday.API.WMS.Models.DataModel.Supplier
+{
+    public class SupplierModel
+    {
+        public SupplierModel()
+        {
+        }
+
+    }
+}
---
+++ API/KKday.API.WMS/KKday.API.WMS/Models/Repository/Product/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using KKday.API.WMS.AppCode.Proxy;
+using KKday.API.WMS.Models.DataModel.Product;
+
+namespace KKday.API.WMS.Models.Repository.Product
+{
+    public class ProductRepository
+    {
+        public ProductRepository()
+        {
+        }
+
+        public static ProductResponseModel GetProdDtl(QueryProdRquestModel queryRQ)
+        {
+            ProductResponseModel product = new ProductResponseModel();
+            var kkapi_prod = ProdProxy.getProd(queryRQ);
+
+
+            return product;
+        }
+    }
+}
