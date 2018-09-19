f454267ffc5f0c73279f0a333b24d75020a535cc
luluchiu016 <lulu.chiu@kkday.com>
2018-09-19 14:58:46 +0800

    add code
    add code

---
+++ API/KKday.API.WMS/KKday.API.WMS/Models/DataModel/Product/QueryProdRquestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace KKday.API.WMS.Models.DataModel.Product
+{
+    public class QueryProdRquestModel
+    {
+        public string b2d_xid { set; get; }
+        public string locale_lang { get; set; }
+        public string current_currency { get; set; }
+        public string prod_no { get; set; }
+        public string query_Type { get; set; }
+    }
+}
