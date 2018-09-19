0e232977e61a3ea4475a0cddbd6447712aa78cd6
Nancy Chiang <nancy.chih@kkday.com>
2018-09-19 17:51:21 +0800

    upd

---
+++ API/KKday.API.WMS/KKday.API.WMS/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KKday.API.WMS.Models.DataModel.Search;
+using KKday.API.WMS.Models.Search;
+using Microsoft.AspNetCore.Mvc;
+using KKday.API.WMS.Models.Repository;
+using KKday.API.WMS.Models.DataModel.Product;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KKday.API.WMS.Controllers {
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class SearchController : Controller {
+
+        [HttpPost]
+        public List<ProductBaseModel> GetProd([FromBody]SearchRQModel list_rq) {
+            var prod_list = new List<ProductBaseModel>();
+            prod_list = ProdRepository.GetProdList(list_rq);
+
+            return prod_list;
+        }
+
+      
+    }
+}
