37c3a19fd2b57f49caf799e59d7930e431593579
Nancy Chiang <nancy.chih@kkday.com>
2018-09-21 15:20:24 +0800

    upd

--- API/KKday.API.WMS/KKday.API.WMS/Controllers/SearchController.cs
+++ API/KKday.API.WMS/KKday.API.WMS/Controllers/SearchController.cs
@@ -10,6 +10,10 @@ using Microsoft.AspNetCore.Mvc;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace KKday.API.WMS.Controllers {
+
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
     public class SearchController : Controller {
 
 
@@ -23,3 +27,4 @@ namespace KKday.API.WMS.Controllers {
         }
     }
 }
+
