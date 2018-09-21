5997c42004bd2f47820bb928efae4d700756792f
luluchiu016 <lulu.chiu@kkday.com>
2018-09-21 14:08:36 +0800

    add
    add

--- API/KKday.API.WMS/KKday.API.WMS/Models/Repository/Product/ProductRepository.cs
+++ API/KKday.API.WMS/KKday.API.WMS/Models/Repository/Product/ProductRepository.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using KKday.API.WMS.AppCode.Proxy;
 using KKday.API.WMS.Models.DataModel.Product;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace KKday.API.WMS.Models.Repository.Product
 {
@@ -10,11 +14,64 @@ namespace KKday.API.WMS.Models.Repository.Product
         {
         }
 
-        public static ProductResponseModel GetProdDtl(QueryProdRquestModel queryRQ)
+        public static ProductModel GetProdDtl(QueryProductModel queryRQ)
         {
-            ProductResponseModel product = new ProductResponseModel();
-            var kkapi_prod = ProdProxy.getProd(queryRQ);
+            ProductModel product = new ProductModel();
 
+            try
+            {
+                JObject obj = ProdProxy.getProd(queryRQ);
+                product.prod_no = obj["content"]["product"]["prodOid"].ToString();
+                product.prod_name = obj["content"]["product"]["productName"].ToString();
+                List<Remind> reminds = null;
+                JArray aa = (JArray)obj["content"]["remindList"];
+                for (int i = 0; i < aa.Count; i++)
+                {
+                    JArray axx = (JArray)aa[i]["remind"];
+                    reminds = axx
+                                .Select(x => new Remind
+                                {
+                                    remind_xid = (int)x["detailOid"],
+                                    remind_desc = (string)x["desc"]
+                                }).ToList();
+                }
+
+                //foreach (var ttt in aa)
+                //{
+                //    reminds = ttt["remind"]
+                //                .Select(x => new Remind
+                //                {
+                //                    remind_xid = (int)x["detailOid"],
+                //                    remind_desc = (string)x["desc"]
+                //                }).ToList();
+
+                //}
+                product.remind_list = reminds;
+
+                List<Remind> remList = new List<Remind>();
+                Remind rem = null;
+
+                if (obj["content"]["remindList"].ToString() != "")
+                {
+                    JArray items = (JArray)obj["content"]["remindList"];
+                    int remindList = items.Count;
+                    for (int i = 0; i < remindList; i++)
+                    {
+                        rem = new Remind();
+                        rem.remind_desc = items[i]["remind"]["desc"].ToString();
+                        rem.remind_xid = (int)items[i]["remind"]["detailOid"];
+                        remList.Add(rem);
+                    }
+                    product.remind_list = remList;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var aa = ex.Message;
+            }
+
+          
 
             return product;
         }
