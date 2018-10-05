17cf81d4103797afdeb60f769530aa47272bf504
Nancy Chiang <nancy.chih@kkday.com>
2018-10-05 11:46:04 +0800

    upd

---
+++ API/KKday.API.WMS/KKday.API.WMS/AppCode/Proxy/PackageProxy.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using KKday.API.WMS.Models.DataModel.Package;
+using KKday.API.WMS.Models.DataModel.Product;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Json = KKday.API.WMS.Models.DataModel.Product.Json;
+
+namespace KKday.API.WMS.AppCode.Proxy {
+    public class PackageProxy {
+        /// <summary>
+        /// Gets the package lst.
+        /// </summary>
+        /// <returns>The package lst.</returns>
+        /// <param name="query_lst">Query lst.</param>
+        //取套餐列表
+        public static JObject getPkgLst(QueryProductModel query_lst) {
+            var obj = new JObject();
+            try {
+                string result = "";
+                using (var handler = new HttpClientHandler()) {
+                    handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+                    handler.ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) => {
+                            return true;
+                        };
+
+                    using (var client = new HttpClient(handler)) {
+                        client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        KKdayApiProdRequestModel RQ = new KKdayApiProdRequestModel() {
+                            apiKey = "kkdayapi",
+                            userOid = "1",
+                            ver = "1.0.1",
+                            locale = query_lst.locale_lang,
+                            currency = query_lst.current_currency,
+                            ipaddress = "1",
+                            json = new Json() {
+                                pkgStatus = query_lst.query_Type,
+                                pkgOid = query_lst.pkg_no,
+                                multipricePlatform = "01"
+                            }
+
+                        };
+
+
+                        string json_data = JsonConvert.SerializeObject(RQ);
+                        string url = $"{Website.Instance.Configuration["URL:KK_PKG"]}{query_lst.prod_no}";
+
+                        using (HttpContent content = new StringContent(json_data)) {
+                            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                            var response = client.PostAsync(url, content).Result;
+                            result = response.Content.ReadAsStringAsync().Result;
+
+                        }
+                    }
+
+                }
+                obj = JObject.Parse(result);
+
+            } catch (Exception ex) {
+                Website.Instance.logger.FatalFormat($"getProd  Error :{ex.Message},{ex.StackTrace}");
+                throw ex;
+            }
+
+            return obj;
+        }
+
+        /// <summary>
+        /// Gets the sale date.
+        /// </summary>
+        /// <returns>The sale date.</returns>
+        /// <param name="query_lst">Query lst.</param>
+        //取套餐可販售日期
+        public static JObject getSaleDate(QueryProductModel query_lst) {
+            var obj = new JObject();
+            try {
+                string result = "";
+                using (var handler = new HttpClientHandler()) {
+                    handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+                    handler.ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) => {
+                            return true;
+                        };
+
+                    using (var client = new HttpClient(handler)) {
+                        client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        KKdayApiProdRequestModel RQ = new KKdayApiProdRequestModel() {
+                            apiKey = "kkdayapi",
+                            userOid = "1",
+                            ver = "1.0.1",
+                            locale = query_lst.locale_lang,
+                            currency = query_lst.current_currency,
+                            ipaddress = "1",
+                            json = new Json() {
+                                prodOid = query_lst.prod_no,
+                                rtnMonth = "2",//只出兩個月的可售日期
+                           
+                            }
+
+                        };
+
+                        string json_data = JsonConvert.SerializeObject(RQ);
+                        string url = Website.Instance.Configuration["URL:KK_PKG_DAY"];
+
+                        using (HttpContent content = new StringContent(json_data)) {
+                            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                            var response = client.PostAsync(url, content).Result;
+                            result = response.Content.ReadAsStringAsync().Result;
+                            }
+                        }
+                    }
+                obj = JObject.Parse(result);
+
+            } catch (Exception ex) {
+                Website.Instance.logger.FatalFormat($"getProd  Error :{ex.Message},{ex.StackTrace}");
+                throw ex;
+            }
+
+            return obj;
+        }
+
+        /// <summary>
+        /// Gets the events.
+        /// </summary>
+        /// <returns>The events.</returns>
+        /// <param name="query_lst">Query lst.</param>
+        //取套餐場次
+        public static JObject getEvents(QueryProductModel query_lst) {
+            var obj = new JObject();
+            try {
+                string result = "";
+                using (var handler = new HttpClientHandler()) {
+                    handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+                    handler.ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) => {
+                            return true;
+                        };
+
+                    using (var client = new HttpClient(handler)) {
+                        client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        KKdayApiProdRequestModel RQ = new KKdayApiProdRequestModel() {
+                            apiKey = "kkdayapi",
+                            userOid = "1",
+                            ver = "1.0.1",
+                            locale = query_lst.locale_lang,
+                            currency = query_lst.current_currency,
+                            ipaddress = "1",
+                            json = new Json() {
+                                prodOid = query_lst.prod_no,
+                                pkgOid = query_lst.pkg_no,
+                                begDt = "",
+                                endDt = ""
+                             
+                            }
+
+                        };
+
+                        string json_data = JsonConvert.SerializeObject(RQ);
+                        string url = Website.Instance.Configuration["URL:KK_PKG_EVENT"];
+
+                        using (HttpContent content = new StringContent(json_data)) {
+                            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                            var response = client.PostAsync(url, content).Result;
+                            result = response.Content.ReadAsStringAsync().Result;
+                        }
+                    }
+                }
+                obj = JObject.Parse(result);
+
+            } catch (Exception ex) {
+                Website.Instance.logger.FatalFormat($"getProd  Error :{ex.Message},{ex.StackTrace}");
+                throw ex;
+            }
+
+            return obj;
+        }
+    }
+}
---
+++ API/KKday.API.WMS/KKday.API.WMS/Controllers/AuthorizeController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using KKday.API.WMS.AppCode.DAL;
+using KKday.API.WMS.AppCode.Proxy;
+
+using KKday.API.WMS.Models.DataModel.Authorize;
+using KKday.API.WMS.Models.DataModel.User;
+using KKday.API.WMS.Models.Repository.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KKday.API.WMS.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizeController : Controller {
+
+        /// <summary>
+        /// The redis cache.
+        /// </summary>
+        private readonly IDistributedCache redisCache;
+
+        public AuthorizeController(IDistributedCache redisCache) {
+            this.redisCache = redisCache;
+        }
+
+        [HttpGet("GetToken")]
+        public ApiUserModel GetToken(string account,string password)
+        {
+            string token = "";
+            ApiUserModel user = new ApiUserModel();
+            try
+            {
+                //1. 從IS4取使用者的門票
+                GetTokenResponseModel response = AuthProxy.getToke(account, password);
+                token = response.access_token ?? response.error_description;
+
+                //2. 從DB抓使用者資訊
+                user = UserRepository.GetApiUser(account, password);
+
+                //3. 把使用者的資訊轉成byte 存進去redis快取
+                var userByte = ObjectToByteArray(user);
+               
+                redisCache.Set("wms.api.token", userByte, 
+                               new DistributedCacheEntryOptions() {
+                    AbsoluteExpiration = DateTime.Now.AddHours(24)
+                    //設定過期時間，時間一到快取立刻就被移除
+                });
+            
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return user;
+        }
+
+        //將物件轉為ByteArray
+        private byte[] ObjectToByteArray(Object aum) {
+            if (aum == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, aum);
+            return ms.ToArray();
+        }
+
+
+
+
+        [HttpGet("authToken")]
+        public string authToken(string token)
+        {
+            string result = "";
+            HttpClient client = new HttpClient();
+        
+            try
+            {
+
+                var cacheBytes = redisCache.Get("wms.api.token");
+
+                if (cacheBytes != null) {
+
+                    token = System.Text.Encoding.UTF8.GetString(cacheBytes);
+
+                    //Token 驗證的語法
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                    HttpResponseMessage response = client.GetAsync("http://192.168.2.83:5009/api/values").Result;
+                    result = response.Content.ReadAsStringAsync().Result;
+
+                } else {
+                    result = "系統閒置過久，請重新登入！";
+                }
+
+            } catch (Exception ex)
+            {
+                result = ex.Message;
+
+            }
+            return result;
+        }
+
+
+
+    }
+}
