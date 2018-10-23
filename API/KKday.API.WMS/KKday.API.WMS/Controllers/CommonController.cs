d2e4739b4232c6368298522ea056af87f88f9d81
john.tseng <john.tseng@kkday.com>
2018-10-23 18:29:51 +0800

    Add CommonController Add currency action etc

---
+++ API/KKday.API.WMS/KKday.API.WMS/Controllers/CommonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KKday.API.WMS.Models.Repository.Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;//IsoDateTimeConverter
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KKday.API.WMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommonController : Controller
+    {
+        /// <summary>
+        /// Currency the specified locale.
+        /// </summary>
+        /// <returns>The currency.</returns>
+        /// <param name="locale">Locale.</param>
+        [HttpGet("Currency")]
+        public JObject Currency(string locale)
+        {
+            JObject currency = new JObject();
+
+            try
+            {
+                Website.Instance.logger.Info($"WMS Currency Start! B2D locale:{locale}");
+
+                currency = CommonRepository.GetCurrency(locale);
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return currency;
+        }
+    }
+}
---
+++ API/KKday.API.WMS/KKday.API.WMS/Models/Repository/Common/CommonRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using KKday.API.WMS.AppCode.DAL;
+using Newtonsoft.Json.Linq;
+
+namespace KKday.API.WMS.Models.Repository.Common
+{
+    public class CommonRepository
+    {
+
+        /// <summary>
+        /// Gets the currency.
+        /// </summary>
+        /// <returns>The currency.</returns>
+        /// <param name="locale">Locale.</param>
+        public static JObject GetCurrency(string locale)
+        {
+
+            JObject result = new JObject();
+            try
+            {
+
+                JObject obj = CommonDAL.GetCurrency(locale);
+
+                if (obj != null && obj.Count > 0)
+                {
+                    result.Add("result","200");
+                    result.Add("result_msg", "OK");
+                    result.Add("currencyList", (JArray)obj["Table"]);
+
+                    JArray currencyList = (JArray)obj["Table"];
+
+                   
+
+
+                }
+                else
+                {
+                    //若找不到該語系的幣別 則回傳找不到的訊息 
+                    result.Add("result", "404");
+                    result.Add("result_msg", "currencyList not found!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Website.Instance.logger.FatalFormat($"getCurrency  Error :{ex.Message},{ex.StackTrace}");
+
+                throw ex;
+
+            }
+
+            return result;
+
+        }
+
+      
+    }
+}
