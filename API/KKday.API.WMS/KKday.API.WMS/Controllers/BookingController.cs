a5c47115d28ee1ae9379abddfacfd6fa212dcbb3
john.tseng <john.tseng@kkday.com>
2018-10-31 18:58:47 +0800

    Add order

--- API/KKday.API.WMS/KKday.API.WMS/Controllers/BookingController.cs
+++ API/KKday.API.WMS/KKday.API.WMS/Controllers/BookingController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using KKday.API.WMS.Models.Repository.Book;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace KKday.API.WMS.Controllers {
+
+    [Route("api/[controller]")]
     public class BookingController : Controller {
-        // GET: /<controller>/
-        public IActionResult Index() {
-            return View();
+
+        [HttpGet("InsertOrder")]
+        public void InsertOrder()
+        {
+            //Website.Instance.logger.Info($"WMS QueryProduct Start! B2D Xid:{queryRQ.company_xid},KKday ProdOid:{queryRQ.prod_no}");
+
+            //var prod_dtl = new ProductModel();
+
+            //prod_dtl = ProductRepository.GetProdDtl(queryRQ);
+
+            BookRepository.InsertOrder();
+
+            //return prod_dtl;
         }
     }
 }
