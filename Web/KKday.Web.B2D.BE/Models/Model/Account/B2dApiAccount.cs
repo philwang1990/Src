c2c82401edf32b7bb81144c6175f79a72bc6580b
唐韻筑 <dora.tang@kkday.com>
2018-11-07 11:24:01 +0800

    upd
    共用model調整

--- Web/KKday.Web.B2D.BE/Models/Model/Account/B2dApiAccount.cs
+++ Web/KKday.Web.B2D.BE/Models/Model/Account/B2dApiAccount.cs
@@ -2,24 +2,10 @@
 namespace KKday.Web.B2D.BE.Models.Model.Account
 {
     [Serializable]
-    public class B2D_API_Account
+    public class B2dApiAccount : B2dAccount
     {
-        public string XID { get; set; }           //帳號流水號
-        public string COMPANY_XID { get; set; }   //所屬公司流水號
-        public string GUID { get; set; }          //連絡信箱
+        //public Int64 COMPANY_XID { get; set; }    //所屬公司流水號
         public string SOURCE { get; set; }        //來源
-
-        public string ACCOUNT_TYPE { get; set; }  //帳號權限(00管理admin/01一般user)
-        public string ENABLE { get; set; }        //是否有效(Y/N)
-
-        public string ACCOUNT { get; set; }       //登入帳號(email)
-        public string PASSWORD { get; set; }      //登入密碼
-
-        public string NAME_FIRST { get; set; }    //姓氏
-        public string NAME_LAST { get; set; }     //名字
-        public string TITLE { get; set; }         //職稱
-        public string DEPARTMENT { get; set; }    //部門
-        public string EMAIL { get; set; }         //連絡信箱
-        public string TEL { get; set; }           //聯絡電話
+        public string TOKEN { get; set; }         //API TOKEN
     }
 }
--- Web/KKday.Web.B2D.BE/Models/Repository/IB2dAccountRepository.cs
+++ Web/KKday.Web.B2D.BE/Models/Repository/IB2dAccountRepository.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using KKday.Web.B2D.BE.Areas.KKday.Models.DataModel.Account;
 using KKday.Web.B2D.BE.Models.Model.Account;
 using KKday.Web.B2D.BE.Models.Model.Common;
-  
+
 namespace KKday.Web.B2D.BE.Models.Repository
 {
     public interface IB2dAccountRepository
     {
         //取得查詢,排序與分頁資料
         QueryParamsModel GetQueryParamModel(string filter, string sorting, int size, int current_page);
+        QueryParamsModel GetQueryParamModel(Int64 comp_xid, string filter, string sorting, int size, int current_page);
+
         //取得分銷商會員清單總筆數
         int GetAccountsCount(string filter);
+
         //取得分銷商會員清單
         List<B2dAccount> GetAccounts(string filter, int skip, int size, string sorting);
+        List<B2dAccount> GetAccounts(Int64 comp_xid, string filter, int skip, int size, string sorting);
+
         //取得會員資料
         B2dAccount GetAccount(Int64 xid);
+
+        void InsertAccount(UserAccount acct, string crt_user);
+        //關閉帳號資料
+        //B2dAccount CloseAccount(Int64 xid);
+
         //更新使用者資料
-        void UpdateAccount(B2dAccoutUpdModel account, string upd_user);
+        void UpdateAccount(UserAccount acct, string upd_user);
+
         //更新使用者密碼
-        bool SetNewPassword(string account, string password);
+        bool SetNewPassword(string acct, string password);
     }
 }
