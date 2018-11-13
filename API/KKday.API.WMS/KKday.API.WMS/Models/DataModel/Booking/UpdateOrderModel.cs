80c91284389fbf829045cc287499eaafcf9f1e38
john.tseng <john.tseng@kkday.com>
2018-11-13 18:33:43 +0800

    appsetting.json

--- API/KKday.API.WMS/KKday.API.WMS/Models/DataModel/Booking/UpdateOrderModel.cs
+++ API/KKday.API.WMS/KKday.API.WMS/Models/DataModel/Booking/UpdateOrderModel.cs
@@ -11,7 +11,7 @@ namespace KKday.API.WMS.Models.DataModel.Booking
         public string order_no { get; set; }
         public string order_oid { get; set; }
         public string order_mid { get; set; }
-        public string company_xid { get; set; }
+        public int company_xid { get; set; }
 
     }
 }
