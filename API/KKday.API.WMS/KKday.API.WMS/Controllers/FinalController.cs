84812fb5a455017a95b043b65652d331d0762730
john.tseng <john.tseng@kkday.com>
2018-11-29 11:07:59 +0800

    Step3 傳入參數調整 多一個 Step3RQModel

--- API/KKday.API.WMS/KKday.API.WMS/Controllers/FinalController.cs
+++ API/KKday.API.WMS/KKday.API.WMS/Controllers/FinalController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using KKday.API.WMS.Models.DataModel.Pmch ;
+using KKday.API.WMS.Models.DataModel.Final;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using KKday.API.WMS.AppCode;
@@ -29,32 +30,32 @@ namespace KKday.API.WMS.Controllers
 
         [HttpPost("Step3")]
         //付款後導回
-        public String Step3(string mid, string jsondata)
+        public String Step3([FromBody]Step3RQModel rq )
         {
             //jsondata = jsondata.Replace(@"\","");
             //回傳的連結有訂編 (記log)
             //透過訂編將redis 的資料抓回送出去的資料
             //取b2dredis 內的paymentDtl
 
-            string payDtlStr = rds.getRedis("b2d:ec:payDtl:" + mid);
+            string payDtlStr = rds.getRedis("b2d:ec:payDtl:" + rq.mid);
             PaymentDtl  payDtl= JsonConvert.DeserializeObject<PaymentDtl>(payDtlStr);
 
             //從kkday redis 取出
             //組出token res:pmgwTransNo, res:pmgwMethod ,res:pmch_resp ceil res:payAmount order_mid
             //md5($pmgw_trans_no.$pmgw_method.$trans_curr_cd.$trans_amt.$pmch_ref_no.$key);
             //PmchSslResponse res = JsonConvert.DeserializeObject<PmchSslResponse>(jsondata); //舊版
-            PmchSslResponse2 res = JsonConvert.DeserializeObject<PmchSslResponse2>(jsondata); //新版
+            //PmchSslResponse2 res = JsonConvert.DeserializeObject<PmchSslResponse2>(rq.jsondata); //新版
             //res.data.pmgw_trans_no = res.data.pmgw_trans_no.Replace(" ", "+");
-            string transNo = GibberishAES.OpenSSLDecrypt(res.data.pmgw_trans_no, Website.Instance.Configuration["PMCH:TRANS_NO"]);
+            string transNo = GibberishAES.OpenSSLDecrypt(rq.jsondata.data.pmgw_trans_no, Website.Instance.Configuration["PMCH:TRANS_NO"]);
             //CallJsonPay req = JsonConvert.DeserializeObject<CallJsonPay>(RedisHelper.getProdInfotoRedis("b2d:ec:pmchSslRequest:" + id)); //using KKday.Web.B2D.EC.AppCode;
-            CallJsonPay2 req = JsonConvert.DeserializeObject<CallJsonPay2>(rds.getRedis("b2d:ec:pmchSslRequest:" + mid)); //using KKday.Web.B2D.EC.AppCode;
+            CallJsonPay2 req = JsonConvert.DeserializeObject<CallJsonPay2>(rds.getRedis("b2d:ec:pmchSslRequest:" + rq.mid)); //using KKday.Web.B2D.EC.AppCode;
 
             string token = Website.Instance.Configuration["PMCH:TOKEN"];
-            string pmgwMethod = res.data.pmgw_method;
+            string pmgwMethod = rq.jsondata.data.pmgw_method;
 
-            string payCurrency = res.data.pay_currency;
-            string payAmount = Math.Ceiling(res.data.pay_amount).ToString();
-            string pmgwValidToken =  MD5Tool.GetMD5(transNo + pmgwMethod + payCurrency + payAmount + mid + token);
+            string payCurrency = rq.jsondata.data.pay_currency;
+            string payAmount = Math.Ceiling(rq.jsondata.data.pay_amount).ToString();
+            string pmgwValidToken =  MD5Tool.GetMD5(transNo + pmgwMethod + payCurrency + payAmount + rq.mid + token);
 
             KKapiHelper helper = new KKapiHelper();
             //必須要再呼叫一次要讓FA 知道這個授權是kkday做的!而不是robot
@@ -66,7 +67,7 @@ namespace KKday.API.WMS.Controllers
                 //如果ok就upd
                 distributorInfo fakeContact = DataSettingRepository.fakeContact();
                 //helper.PayUpdSuccessUpdOrder(id, transNo, payDtl, req, res, fakeContact);//舊版
-                helper.PayUpdSuccessUpdOrder2(mid, transNo, payDtl, req, res, fakeContact); //新版
+                helper.PayUpdSuccessUpdOrder2(rq.mid, transNo, payDtl, req, rq.jsondata, fakeContact); //新版
             } 
 
             return result;
