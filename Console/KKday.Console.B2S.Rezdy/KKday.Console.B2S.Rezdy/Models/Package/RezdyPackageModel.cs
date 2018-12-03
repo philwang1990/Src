4d0eef95d39e25f2dbac08f296bf7bd142a352cb
snow.choy <snow.choy@kkday.com>
2018-12-03 18:56:02 +0800

    commit

---
+++ Console/KKday.Console.B2S.Rezdy/KKday.Console.B2S.Rezdy/Models/Package/RezdyPackageModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KKday.Consoles.B2S.Rezdy.Models.Package
+{
+    public class RezdyPackageModel
+    {
+        public requestStatus RequestStatus { get; set; }
+        public List<sessions> Sessions { get; set; }
+    }
+
+    public class requestStatus
+    {
+        public bool success { get; set; }
+        public string version { get; set; }
+    }
+
+    public class sessions
+    {
+        public long id { get; set; }
+
+        public string productCode { get; set; }
+    }
+}
