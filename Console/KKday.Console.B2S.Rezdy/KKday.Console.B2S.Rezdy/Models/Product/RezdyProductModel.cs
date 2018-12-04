49e6eeac3029345ed1488e3380bdd04fa8d813f8
john.tseng <john.tseng@kkday.com>
2018-12-04 15:40:12 +0800

    RezdyProductModel 完成

--- Console/KKday.Console.B2S.Rezdy/KKday.Console.B2S.Rezdy/Models/Product/RezdyProductModel.cs
+++ Console/KKday.Console.B2S.Rezdy/KKday.Console.B2S.Rezdy/Models/Product/RezdyProductModel.cs
@@ -6,7 +6,7 @@ namespace KKday.Consoles.B2S.Rezdy.Models.Produvt
     public class RezdyProductModel
     {
         public requestStatus RequestStatus { get; set; }
-        public List<products> products { get; set; }
+        public List<products> Products { get; set; }
     }
 
     public class requestStatus
@@ -18,7 +18,87 @@ namespace KKday.Consoles.B2S.Rezdy.Models.Produvt
     public class products
     {
         public string productType { get; set; }
-
         public string name { get; set; }
+        public string shortDescription { get; set; }
+        public string description { get; set; }
+        public string productCode { get; set; }
+        public string internalCode { get; set; }
+        public long supplierId { get; set; }
+        public string supplierAlias { get; set; }
+        public string supplierName { get; set; }
+        public string timezone { get; set; }
+        public double advertisedPrice { get; set; }
+        public List<priceOptions> PriceOptions { get; set; }
+
+        public string currency { get; set; }
+        public string unitLabel { get; set; }
+        public string unitLabelPlural { get; set; }
+        public bool quantityRequired { get; set; }
+        public long quantityRequiredMin { get; set; }
+        public long nquantityRequiredMaxame { get; set; }
+        public List<images> Images { get; set; }
+
+        public string bookingMode { get; set; }
+        public bool charter { get; set; }
+        public string terms { get; set; }
+        public string generalTerms { get; set; }
+        public List<bookingFields> BookingFields { get; set; }
+
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string confirmMode { get; set; }
+        public long confirmModeMinParticipants { get; set; }
+        public string agentPaymentType { get; set; }
+        public long maxCommissionPercent { get; set; }
+        public bool commissionIncludesExtras { get; set; }
+        public DateTime dateCreated { get; set; }
+        public long minimumNoticeMinutes { get; set; }
+        public long durationMinutes { get; set; }
+        public DateTime dateUpdated { get; set; }
+        public long pickupId { get; set; }
+        public locationAddress LocationAddress { get; set; }
+
+        public List<String> Languages { get; set; }
+        public List<String> Tags { get; set; }
+
     }
+
+    public class priceOptions
+    {
+        public long price { get; set; }
+        public string label { get; set; }
+        public long id { get; set; }
+        public long seatsUsed { get; set; }
+        public string productCode { get; set; }
+    }
+
+    public class images
+    {
+        public long id { get; set; }
+        public string itemUrl { get; set; }
+        public string thumbnailUrl { get; set; }
+        public string mediumSizeUrl { get; set; }
+        public string largeSizeUrl { get; set; }
+    }
+
+    public class bookingFields
+    {
+        public string label { get; set; }
+        public bool requiredPerParticipant { get; set; }
+        public bool requiredPerBooking { get; set; }
+        public bool visiblePerParticipant { get; set; }
+        public bool visiblePerBooking { get; set; }
+        public string fieldType { get; set; }
+    }
+
+    public class locationAddress
+    {
+        public string addressLine { get; set; }
+        public string city { get; set; }
+        public string countryCode { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+   
 }
--- Console/KKday.Console.B2S.Rezdy/KKday.Console.B2S.Rezdy/ProductRepository.cs
+++ Console/KKday.Console.B2S.Rezdy/KKday.Console.B2S.Rezdy/ProductRepository.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using log4net;
 using KKday.Consoles.B2S.Rezdy.AppCode;
-using KKday.Consoles.B2S.Rezdy.Models.Package;
+using KKday.Consoles.B2S.Rezdy.Models.Produvt;
 using Newtonsoft.Json;
 
 namespace KKday.Consoles.B2S.Rezdy.ProductRepository
@@ -17,10 +17,10 @@ namespace KKday.Consoles.B2S.Rezdy.ProductRepository
                 //initial log4net
                 CommonTool.LoadLog4netConfig();
 
-                var get = CommonTool.GetData("https://api.rezdy.com/latest/availability?apiKey=0b3d137cc1db4108a92c309fa7d7f6da&productCode=PVVRFE&startTimeLocal=2018-11-01 00:00:00&endTimeLocal=2019-12-31 00:00:00");
-                //RezdyPackageModel
+                var get = CommonTool.GetData("https://api.rezdy.com/latest/products/marketplace?apiKey=0b3d137cc1db4108a92c309fa7d7f6da&limit=1&supplierId=21470&productCode=P0KEEN");
+                //RezdyProductModel
 
-                RezdyPackageModel obj = JsonConvert.DeserializeObject<RezdyPackageModel>(get);
+                RezdyProductModel obj = JsonConvert.DeserializeObject<RezdyProductModel>(get);
 
                 //do something
             }
