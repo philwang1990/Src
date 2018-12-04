0ed28dff7f2b5570a6fddea15f1a7d74344f2b46
john.tseng <john.tseng@kkday.com>
2018-12-04 16:41:27 +0800

    add productModel

--- Console/KKday.PMS.B2S/KKday.PMS.B2S/ProductRepository.cs
+++ Console/KKday.PMS.B2S/KKday.PMS.B2S/ProductRepository.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using log4net;
 using KKday.PMS.B2S.AppCode;
-using KKday.PMS.B2S.Models.Package;
+using KKday.PMS.B2S.Models.Product;
 using Newtonsoft.Json;
 
 namespace KKday.PMS.B2S.ProductRepository
@@ -17,10 +17,10 @@ namespace KKday.PMS.B2S.ProductRepository
                 //initial log4net
                 CommonTool.LoadLog4netConfig();
 
-                var get = CommonTool.GetData("https://api.rezdy.com/latest/availability?apiKey=0b3d137cc1db4108a92c309fa7d7f6da&productCode=PVVRFE&startTimeLocal=2018-11-01 00:00:00&endTimeLocal=2019-12-31 00:00:00");
-                //RezdyPackageModel
+                var get = CommonTool.GetData("https://api.rezdy.com/latest/products/marketplace?apiKey=0b3d137cc1db4108a92c309fa7d7f6da&limit=1&supplierId=21470&productCode=P0KEEN");
+                //RezdyProductModel
 
-                RezdyPackageModel obj = JsonConvert.DeserializeObject<RezdyPackageModel>(get);
+                RezdyProductModel obj = JsonConvert.DeserializeObject<RezdyProductModel>(get);
 
                 //do something
             }
