50496dfa9bff270a2794ea1b55e5aa9709452351
john.tseng <john.tseng@kkday.com>
2018-12-04 14:22:44 +0800

    add RezdyProductModel

---
+++ Console/KKday.Console.B2S.Rezdy/KKday.Console.B2S.Rezdy/Models/Product/RezdyProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KKday.Consoles.B2S.Rezdy.Models.Produvt
+{
+    public class RezdyProductModel
+    {
+        public requestStatus RequestStatus { get; set; }
+        public List<products> products { get; set; }
+    }
+
+    public class requestStatus
+    {
+        public bool success { get; set; }
+        public string version { get; set; }
+    }
+
+    public class products
+    {
+        public string productType { get; set; }
+
+        public string name { get; set; }
+    }
+}
